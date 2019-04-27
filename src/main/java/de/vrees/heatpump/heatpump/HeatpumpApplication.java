d5a6d69a90ddf20fa50016cd3a0fb82b47e5feb2
vrees <viktor.rees@gmx.de>
2019-04-27 09:00:49 +0200

    git init

---
+++ src/main/java/de/vrees/heatpump/heatpump/HeatpumpApplication.java
@@ -0,0 +1,13 @@
+package de.vrees.heatpump.heatpump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatpumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatpumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/vrees/heatpump/heatpump/HeatpumpApplicationTests.java
@@ -0,0 +1,16 @@
+package de.vrees.heatpump.heatpump;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeatpumpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
